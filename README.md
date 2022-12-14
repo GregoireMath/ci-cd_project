12f3c1e4237827bd0421f31977bdfc5b31754918
user <gregoire.mathian@mines-ales.org>
2022-12-14 15:23:31 +0100

    Final commit on dev_app

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # ci-cd_project
+
+Requirements
+
+python3     --> sudo apt install python3
+pip3        --> python -m pip3 install --upgrade pip
+flask       --> pip3 install flask
+requests    --> pip3 install requests
+
+Setup
+
+git clone git@github.com:GregoireMath/ci-cd_project.git
+cd ci-cd_project
+python3 app.py
+
+Website access
+
+http://127.0.0.1:5000
--- app.py
+++ app.py
@@ -1,18 +1,39 @@
 from flask import Flask,render_template,request,redirect,url_for
+from dotenv import load_dotenv
+import os
+import requests
+import random
 
 app = Flask(__name__)
+load_dotenv()
+key = os.environ.get('API_KEY')
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/mars',methods=['GET'])
 def mars():
-	return render_template('mars.html')
+    url='https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key='+key
+    response = requests.get(url).json()
+
+    links = []
+    for i in response["photos"]:
+        links.append(i['img_src'])
+
+    dice = random.randint(0,len(links))
+    picture = links[dice]
+
+    return render_template('mars.html',picture=picture)
 
 @app.route('/potd',methods=['GET'])
 def potd():
-	return render_template('potd.html')
+    url='https://api.nasa.gov/planetary/apod?api_key='+key
+    response = requests.get(url).json()
+
+    picture = response["hdurl"]
+    text = response["explanation"]
+    return render_template('potd.html',picture=picture,text=text)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
