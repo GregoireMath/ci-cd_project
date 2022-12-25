7b1a5bec888969c52dd33a84de2831de92407d2e
user <gregoire.mathian@mines-ales.org>
2022-12-25 17:43:06 +0100

    Add GitHubActions

--- app_test.py
+++ app_test.py
@@ -1,33 +1,25 @@
+import pytest
 import requests
+from app import app
 
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    client = app.test_client()
 
-def test_http_request_menu():
-    url = "http://localhost:3232/"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.status_code
-    else:
-        raise Exception("Page error on /")
+    yield client
 
+def test_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'index.html' in response.data
 
-def test_http_request_potd():
-    url = "http://localhost:3232/potd"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.status_code
-    else:
-        raise Exception("Page error on /potd")
+def test_mars(client):
+    response = client.get('/mars')
+    assert response.status_code == 200
+    assert b'mars.html' in response.data
 
-
-def test_http_request_mars():
-    url = "http://localhost:3232/mars"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.status_code
-    else:
-        raise Exception("Page error on /mars")
-
-
-test_http_request_mars()
-test_http_request_potd()
-test_http_request_menu()
+def test_potd(client):
+    response = client.get('/potd')
+    assert response.status_code == 200
+    assert b'potd.html' in response.data
